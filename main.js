b87e1490608cd6ce21c4cd1e826bb3b7c67f62c5
Colton <allen.colton@hotmail.com>
2023-02-09 22:10:46 -0800

    added terms and conditions popup

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('testing...')
+function toggleTerms() {
+  document.querySelector('html').dataset.termsDialogIsOpen = document.querySelector('html').dataset.termsDialogIsOpen == "false"
+  
+  document.querySelector('html').dataset.termsDialogIsOpen == "true" ? 
+    document.querySelector('dialog#terms').setAttribute('open', true) : 
+    document.querySelector('dialog#terms').removeAttribute('open')
+}
+
+// [
+//   document.querySelector('.terms-overlay'),
+//   document.querySelector('dialog#terms #close-btn')
+// ].forEach(el => el.addEventListener('click', toggleTerms))
+
+document.querySelector('.terms-overlay').addEventListener('click', toggleTerms)
+document.querySelector('dialog#terms #close-btn')
+.addEventListener('click', toggleTerms)
